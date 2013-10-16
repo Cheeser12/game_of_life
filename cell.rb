be80be2b8cb90a0309cc56cb9ea1e50761fa4a06
Max Mays <cheeser12@ezen.com>
2013-10-16 12:42:36 -0400

    Cell now updates based on the rules of Game of Life

--- cell.rb
+++ cell.rb
@@ -12,4 +12,19 @@ class Cell
             "-"
         end
     end
+    
+    def update(neighbors)
+        n_alive = 0
+        
+        neighbors.each do |cell|
+            n_alive += 1 if cell.alive
+        end
+
+        if @alive then
+            @alive = false if (n_alive < 2 || n_alive > 3)
+        else
+            @alive = true if (n_alive == 3)            
+        end
+        @alive
+    end
 end
